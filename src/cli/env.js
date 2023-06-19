b08d23e8365dd5ca373284a6d524a6e910b3fb47
Serg <martynenko1981@gmail.com>
2023-06-19 14:41:50 +0300

    feat: add env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+import { env } from 'node:process';
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const PREFIX = 'RSS_';
+    const EQUAL = '=';
+    const SEMICOLON = '; ';
+    let output = '';
+    for (let key in env) {
+        if (key.indexOf(PREFIX) != -1)
+            output += SEMICOLON + key + EQUAL + env[key];
+    }
+    console.log(output.slice(2));
 };
 
+env.RSS_news = 123;
+env.RSS_port = 8000;
+env.RSS_name = 'node';
 parseEnv();
