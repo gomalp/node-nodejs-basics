ab563862c2a042c4219a339cfa70951a5a6316c6
Serg <martynenko1981@gmail.com>
2023-06-19 16:41:51 +0300

    feat: add args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+import { argv } from 'node:process';
+// node args.js --propName value --prop2Name value2
 const parseArgs = () => {
     // Write your code here 
+    const IS = ' is ';
+    const COMMAND = '--';
+    const COMMA = ', ';
+    let output = '';
+    for (let i = 0; i < argv.length; i++) {
+        if (argv[i].indexOf(COMMAND) == 0)
+            output += COMMA + argv[i] + IS + argv[++i];
+    }
+    console.log(output.slice(2));
 };
 
 parseArgs();
