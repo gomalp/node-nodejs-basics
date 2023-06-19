a096830c892383861af7e9f94d4cd42803d5412e
Serg <martynenko1981@gmail.com>
2023-06-20 02:23:42 +0300

    feat: add streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+import path from 'path';
+import url from 'url';
+
 const read = async () => {
     // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const DIR = __dirname + '/';
+    const input = createReadStream(DIR + '/files/fileToRead.txt');
+    input.pipe(stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { stdin } from 'node:process';
+import path from 'path';
+import url from 'url';
 const write = async () => {
     // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const DIR = __dirname + '/';
+    const input = createReadStream(stdin);
+    const output = createWriteStream(DIR + '/files/fileToWrite.txt');
+    input.pipe(output);
 };
 
 await write();
